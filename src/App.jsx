0ad97c778f3a3d98e501ca8f66439d15ea3cee3f
ReactedTitan880 <thomasnardou@icloud.com>
2024-07-08 20:07:36 +0200

    change app theme and add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
-import { useState } from 'react'
-import SquareComponent from './buttonComponent'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+
+  const [agents, setAgents] = useState([]);
+
+  axios.get("https://valorant-api.com/v1/agents").then((response) => {
+    setAgents(response.data.data);
+    // console.log(response.data.data);
+  });
+
   return (
     <>
-      <div className="board-row">
-        <SquareComponent />
-        <SquareComponent />
-        <SquareComponent />
-      </div>
 
-      <div className="board-row">
-        <SquareComponent />
-        <SquareComponent />
-        <SquareComponent />
-      </div>
-
-      <div className="board-row">
-        <SquareComponent />
-        <SquareComponent />
-        <SquareComponent />
-      </div>
+      {
+        agents.map((agent) => {
+          return (
+            <div key={agent.uuid} className={`bg-gradient-to-r from-[#${agent.backgroundGradientColors[0]}] to-blue-500 border-8 border-sky-600 w-80 m-10`} >
+              <h1>{agent.displayName}</h1>
+              <img src={agent.displayIcon} alt={agent.displayName} />
+            </div>
+          );
+        })
+      }
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
